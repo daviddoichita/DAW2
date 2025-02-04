2c4acbeddc8fc10afb3fd28b5363aee725c6bb58
daviddoichita <daviddoichita@proton.me>
2025-02-04 17:16:31 +0100

    Operacion de borrado

--- dws/dws-u7-Doichita-David/src/main/java/dav/doi/server/web/webservice/ClienteRestController.java
+++ dws/dws-u7-Doichita-David/src/main/java/dav/doi/server/web/webservice/ClienteRestController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -80,4 +81,14 @@ public class ClienteRestController {
             return new ResponseEntity<>(clienteDTO, HttpStatus.OK);
         }
     }
+
+    @DeleteMapping(path = "/{idCliente}")
+    public ResponseEntity<String> delete(@PathVariable Long idCliente) {
+        log.info(this.getClass().getSimpleName() + " delete: borrar cliente con id: {}", idCliente);
+
+        this.clienteService.deleteById(idCliente);
+
+        return new ResponseEntity<>("Cliente borrado correctamente", HttpStatus.OK);
+    }
+
 }
